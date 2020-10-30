2896342e09b0c9bb3e18941e8b7c7875035cc9be
Gorny <agorny@uiowa.edu>
2020-10-29 23:01:58 -0500

    Completed Static Site
    Code meets requirements for font-styles, responsiveness, layout and color. Might come back to check for bugs and errors.

--- app.py
+++ app.py
@@ -7,9 +7,6 @@ app = Flask(__name__)
 def index():
     return render_template('index.html', pageTitle='Homepage')
 
-@app.route('/aidan')
-def aidan():
-    return render_template('aidan.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
